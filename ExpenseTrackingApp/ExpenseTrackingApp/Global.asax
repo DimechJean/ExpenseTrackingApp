6d726ece4f4d02aaa960b226eaa9c77201cb5006
AxelAzzopardi <axelazzopardi97@gmail.com>
2017-03-21 12:29:29 +0100

    Creation of Project

---
+++ ExpenseTrackingApp/ExpenseTrackingApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExpenseTrackingApp.MvcApplication" Language="C#" %>
---
+++ ExpenseTrackingApp/ExpenseTrackingApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExpenseTrackingApp.Startup))]
+namespace ExpenseTrackingApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
