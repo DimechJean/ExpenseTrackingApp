79d1736bb1f8b10abab3cd2138d87a63d2ad3f04
Jean <Jean@JEAN>
2017-05-03 09:43:53 +0200

    Changes mostly concerning User Interface Design - Done by Axel Azzopardi.

---
+++ ExpenseTrackingApp/ExpenseTrackingApp/Scripts/javascript.js
@@ -0,0 +1,14 @@
+﻿function myFunction() {
+    var $this = $(this);
+
+    $('#fh5co-offcanvass').toggleClass('fh5co-awake');
+    if ($(window).width() < 1600) {
+        $('#fh5co-page, #fh5co-menu').toggleClass('fh5co-sleep');
+    }
+
+    if ($('#fh5co-offcanvass').hasClass('fh5co-awake')) {
+        $this.addClass('active');
+    } else {
+        $this.removeClass('active');
+    }
+}
